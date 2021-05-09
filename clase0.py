faa44d6d4267baf1a80f2348f733c981111e007d
Anthony Salvador <anthonysalvador.francisco@gmail.com>
2021-05-09 17:33:38 -0600

    clase0

--- clase0.py
+++ clase0.py
@@ -1,1 +1,9 @@
 print("Hola mundo")
+# Python 3: Serie de Fibonacci hasta n
+def  fib ( n ):
+    a , b  =  0 , 1
+    mientras que  a  <  n :
+        imprimir ( a , end = '' )
+        a , b  =  b , a + b
+    imprimir ()
+fib ( 1000 )
